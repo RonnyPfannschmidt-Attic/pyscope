ae466fd5e2c09fcbb1d833c6e1c18443c90c7d5d
Ronny Pfannschmidt <opensource@ronnypfannschmidt.de>
2009-05-24 15:40:18 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 if __name__=='__main__':setup(
         name='pyscope',
-        version='0.2',
+        version='0.3',
         author="Ronny Pfannschmidt",
         author_email="Ronny.Pfannschmidt@gmx.de",
         description="a simple lib for lisp-style scoped global variables",
